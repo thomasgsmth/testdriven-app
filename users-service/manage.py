c0f5deac2b5a2a9ef2d561a7339dc70eed3d83d8
Tom Smith <thomasgsmth@yahoo.com>
2017-12-11 16:04:31 -0600

    Postgres Setup

--- users-service/manage.py
+++ users-service/manage.py
@@ -4,20 +4,33 @@
 #   http://flask.pocoo.org/docs/0.12/cli/
 #   https://stackoverflow.com/questions/42754341/use-flasks-click-cli-with-the-app-factory-pattern
 
-from flask.cli import FlaskGroup
-from project import app
+#from flask.cli import FlaskGroup
+from flask_script import Manager
+from project import app, db
 
 
-def shell_context():
-    return {'app': app}
+#def shell_context():
+#    return {'app': app}
 
-app.shell_context_processor(shell_context)
+#app.shell_context_processor(shell_context)
 
-cli = FlaskGroup(app)
+#cli = FlaskGroup(app)
 
-@cli.command
-def custom_comment():
-    pass
+#@cli.command
+#def custom_comment():
+#    pass
+
+manager = Manager(app)
+
+
+@manager.command
+def recreate_db():
+    """Recreates a database."""
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+    
 
 if __name__ == '__main__':
-    cli()
+    manager.run()
+#    cli()
--- users-service/project/__init__.py
+++ users-service/project/__init__.py
@@ -1,14 +1,56 @@
 # users-service/project/__init__.py
 
 
+import os
+import datetime
+import sys
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 
 # instantiate the app
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+# model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True,
+        )
+    username = db.Column(
+        db.String(128),
+        nullable=False,
+        )
+    email = db.Column(
+        db.String(128),
+        nullable=False,
+        )
+    active = db.Column(
+        db.Boolean(),
+        default=True,
+        nullable=False,
+        )
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+        
+# Two options to view app.config in logs
+# 1 - Write to stderr
+#print(app.config, file=sys.stderr)
+
+# 2 - Write to stdout and then flush
+#print(app.config)
+#sys.stdout.flush()
 
 
 @app.route('/users/ping', methods=['GET'])
