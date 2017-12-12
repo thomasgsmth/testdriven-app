ed3608ec84507893e802c2c42a432d10740ce443
Tom Smith <thomasgsmth@yahoo.com>
2017-12-12 09:13:11 -0600

    Flask Blueprints refactor

--- users-service/project/__init__.py
+++ users-service/project/__init__.py
@@ -2,60 +2,28 @@
 
 
 import os
-import datetime
-import sys
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 
-# instantiate the app
-app = Flask(__name__)
-
-# set config
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
-
 # instantiate the db
-db = SQLAlchemy(app)
-
-# model
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(
-        db.Integer,
-        primary_key=True,
-        autoincrement=True,
-        )
-    username = db.Column(
-        db.String(128),
-        nullable=False,
-        )
-    email = db.Column(
-        db.String(128),
-        nullable=False,
-        )
-    active = db.Column(
-        db.Boolean(),
-        default=True,
-        nullable=False,
-        )
-
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-        
-# Two options to view app.config in logs
-# 1 - Write to stderr
-#print(app.config, file=sys.stderr)
-
-# 2 - Write to stdout and then flush
-#print(app.config)
-#sys.stdout.flush()
+db = SQLAlchemy()
 
 
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!',
-    })
+def create_app():
+
+    # instantiate the app
+    app = Flask(__name__)
+
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    return app
