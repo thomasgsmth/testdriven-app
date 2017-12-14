0f051310c505bf1758609bed04c50624ab745569
Tom Smith <thomasgsmth@yahoo.com>
2017-12-14 10:00:42 -0600

    Get all users

--- users-service/project/tests/test_users.py
+++ users-service/project/tests/test_users.py
@@ -10,6 +10,12 @@ from project import db
 from project.api.models import User
 
 
+def add_user(username, email):
+    user = User(username=username, email=email)
+    db.session.add(user)
+    db.session.commit()
+    return user
+
 class TestUserServices(BaseTestCase):
     """Tests for the Users Service"""
 
@@ -89,9 +95,7 @@ class TestUserServices(BaseTestCase):
 
     def test_single_user(self):
         """Ensure get single user behanves correctly."""
-        user = User(username='michael', email='michael@realpython.com')
-        db.session.add(user)
-        db.session.commit()
+        user = add_user('michael', 'michael@realpython.com')
         with self.client:
             response = self.client.get(f'users/{user.id}')
             data = json.loads(response.data.decode())
@@ -118,6 +122,21 @@ class TestUserServices(BaseTestCase):
             self.assertIn('User does not exist', data['message'])
             self.assertIn('fail', data['status'])
 
+    def test_all_users(self):
+        """Ensure get all users behaves correctly."""
+        add_user('michael', 'michael@realpython.com')
+        add_user('fletcher', 'fletcher@realpython.com')
+        with self.client:
+            response = self.client.get('/users')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(len(data['data']['users']), 2)
+            self.assertIn(
+                'michael@realpython.com', data['data']['users'][0]['email'])
+            self.assertIn('fletcher', data['data']['users'][1]['username'])
+            self.assertIn(
+                'fletcher@realpython.com', data['data']['users'][1]['email'])
+            self.assertIn('success', data['status'])
             
 if __name__ == '__main__':
     unittest.main()
